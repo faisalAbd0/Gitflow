fe1443566f654f6dca76fa0f689a990dbd97fb42
faisalAbd0 <faislabdu277@gmail.com>
2025-01-13 01:51:17 +0300

    prevent divied by zero edited

--- Calclator.java
+++ Calclator.java
@@ -14,7 +14,7 @@ public class Calculator {
 
     public int divide(int a, int b) {
          if (b == 0)
-            throw new IllegalArgumentException("Cannot divide by zero");
+            Sytem.out.println("Error: Division by zero");
         return a / b;
     }
 
